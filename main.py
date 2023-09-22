60a48abb272636f2d0680a1f995bd4d43448bd81
modeweghansenb <modeweghansenb@educ.college-montreal.qc.ca>
2023-09-22 10:04:20 -0400

    Word_counter

---
+++ main.py
@@ -0,0 +1,10 @@
+# Blix Modeweg-hansen
+# 2007
+# WORD_COUNTER
+
+def count_word():  # définir la fonction pour que le programme compte des mots
+    texte = str(input('Mettre une phrase et je vous direz le nombre de mots:'))  # entrer la phrase voulu
+    return print(len(texte.split()))
+
+
+count_word()
